79070290a153c5eb40a9abd32e8e7c55e18b5b15
Emiliano Merelez <emimerelez@gmail.com>
2026-03-26 11:13:16 -0300

    chore: inject ReviewRepository into ReviewsService

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Review } from './entities/review.entity';
+import { Repository } from 'typeorm';
 // import { UpdateReviewDto } from './dto/update-review.dto';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewRepository: Repository<Review>,
+    // necesitamos vincular la review
+  ) {}
+}
