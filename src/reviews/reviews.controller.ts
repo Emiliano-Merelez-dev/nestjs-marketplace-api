b963225cdedf05d1436ffc31ad62928bd73b4c1a
Emiliano Merelez <emimerelez@gmail.com>
2026-03-30 22:33:32 -0300

    feat: expose findAll endpoint in ReviewsController

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 // import { UpdateReviewDto } from './dto/update-review.dto';
@@ -10,5 +10,10 @@ export class ReviewsController {
   @Post()
   create(@Body() createReviewDto: CreateReviewDto) {
     return this.reviewsService.create(createReviewDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.reviewsService.findAll();
   }
 }
