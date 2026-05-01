38e1ddb971a8639cb5e4776879bf791659f32b77
Emiliano Merelez <emimerelez@gmail.com>
2026-05-01 14:48:59 -0300

    docs(payments): add swagger api property and example to CreatePaymentDto

--- src/payments/dto/create-payment.dto.ts
+++ src/payments/dto/create-payment.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreatePaymentDto {
+  @ApiProperty({
+    example: '98H60553TS287434D',
+    description: 'The PayPal Transaction ID received from the frontend/PayPal',
+  })
   @IsString()
   transactionId: string;
 }
