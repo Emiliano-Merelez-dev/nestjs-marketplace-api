3fcffe39fd4cd8cd8c6ecf462acf7d711cd4d562
Emiliano Merelez <emimerelez@gmail.com>
2026-05-04 22:08:12 -0300

    docs(reviews): add Swagger OpenAPI annotations to review entity  and DTOs

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNumber,
   IsOptional,
@@ -9,16 +10,31 @@ import {
 } from 'class-validator';
 
 export class CreateReviewDto {
+  @ApiProperty({
+    example: 5,
+    description: 'Product rating score (from 1 to 5)',
+    minimum: 1,
+    maximum: 5,
+  })
   @IsNumber()
   @Min(1)
   @Max(5)
   @IsPositive()
   rating: number;
 
+  @ApiProperty({
+    example: 'Awesome product, highly recommended!',
+    description: 'User feedback or opinion about the product',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   comment?: string;
 
+  @ApiProperty({
+    example: 'cd533345-f1f3-48c9-a62e-7dc2da50c8f8',
+    description: 'The unique UUID of the product being reviewed',
+  })
   @IsUUID('4', { message: 'a valid productID' })
   productId: string;
 }
--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -10,27 +10,48 @@ import {
   ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'reviews' })
 export class Review {
+  @ApiProperty({
+    example: 'd290f1ee-6c54-4b01-90e6-d701748f0851',
+    description: 'Review ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 4.5,
+    description: 'Product rating score',
+    default: 0,
+  })
   @Column('float', {
     default: 0,
   })
   rating: number;
 
+  @ApiProperty({
+    example: 'This product exceeded my expectations, very high quality.',
+    description: 'User feedback comment',
+  })
   @Column('text')
   comment: string;
 
+  @ApiProperty({
+    example: '2026-04-28T18:00:00Z',
+    description: 'Creation date',
+  })
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
+  @ApiProperty({ type: () => UserEntity })
   @ManyToOne(() => UserEntity, (user) => user.reviews, { nullable: false })
   @JoinColumn({ name: 'user_id' })
   user: User;
 
+  @ApiProperty({ type: () => ProductEntity })
   @ManyToOne(() => ProductEntity, (product) => product.reviews, {
     nullable: false,
     onDelete: 'CASCADE',
