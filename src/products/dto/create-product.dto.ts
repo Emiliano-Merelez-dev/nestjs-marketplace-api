785f694d74d9148027c587e526509b5eaae1871b
Emiliano Merelez <emimerelez@gmail.com>
2026-05-07 11:46:34 -0300

    docs(products): add Swagger OpenAPI annotations to product entity and DTOs

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import {
   IsArray,
   IsIn,
@@ -10,44 +12,77 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title (unique)',
+    nullable: false,
+    minLength: 1,
+    example: 'Nike air Max 2026',
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({ example: 120.5 })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty({
+    example: ['M', 'L', 'XL'],
+    description: 'Available sizes',
+    isArray: true,
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({
+    enum: ['men', 'women', 'kid', 'unisex'],
+    example: 'men',
+  })
   @IsString()
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty({
+    example: ['running', 'shoes', 'sport'],
+    isArray: true,
+    required: false,
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags?: string[];
 
+  @ApiProperty({
+    description: 'ID or name of the category',
+    example: 'Footwear',
+  })
   @IsString()
   category: string;
 
+  @ApiProperty({
+    example: ['https://image1.jpg', 'https://image2.jpg'],
+    isArray: true,
+    required: false,
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -13,17 +13,32 @@ import {
 } from 'typeorm';
 import { ProductImage } from './product-image.entity';
 import { OrderItem } from 'src/orders/entities/order-item.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: 'cd533345-f1f3-48c9-a62e-7dc2da50c8f8',
+    description: 'Product ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'T-Shirt Teslo',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: 0,
+    description: 'Product price',
+  })
   @Column('numeric', {
     precision: 10,
     scale: 2,
@@ -35,55 +50,95 @@ export class Product {
   })
   price: number;
 
+  @ApiProperty({
+    example: 'Anim reprehenderit nulla in anim mollit minim irure commodo.',
+    description: 'Product description',
+    default: null,
+  })
   @Column({
     type: 'text',
     nullable: true,
   })
   description: string;
 
+  @ApiProperty({
+    example: 't_shirt_teslo',
+    description: 'Product SLUG - for SEO',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product stock',
+    default: 0,
+  })
   @Column('int', {
     default: 0,
   })
   stock: number;
 
+  @ApiProperty({
+    example: ['M', 'XL', 'XXL'],
+    description: 'Product sizes',
+    isArray: true,
+  })
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
+  @ApiProperty({
+    example: 'women',
+    description: 'Product gender',
+  })
   @Column('text', {
     array: true,
     default: [],
   })
   gender: string[];
 
+  @ApiProperty()
   @Column('text', {
     array: true,
     default: [],
   })
   tags: string[];
 
+  @ApiProperty({
+    type: () => ProductImage,
+    isArray: true,
+    description: 'Product images',
+  })
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
   })
   images?: ProductImage[];
 
+  @ApiProperty()
   @OneToMany(() => Review, (review) => review.product, { cascade: true })
   reviews: Review[];
 
+  @ApiProperty()
   @OneToMany(() => OrderItem, (orderItem) => orderItem.product)
   orderItems: OrderItem[];
 
+  @ApiProperty({
+    type: () => User,
+    description: 'User who created the product',
+  })
   @ManyToOne(() => User, (user) => user.product, { eager: true })
   @JoinColumn({ name: 'user_id' })
   user: User;
 
+  @ApiProperty({
+    type: () => Category,
+    description: 'Category assigned to the product',
+  })
   @ManyToOne(() => Category, (category) => category.products, { eager: true })
   @JoinColumn({ name: 'category_id' }) // <--- CONECTA CON LA COLUMNA category_id
   category: Category;
