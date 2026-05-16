f9e115d4f1df0a7c2b458995fa34981fa29f9a44
Emiliano Merelez <emimerelez@gmail.com>
2026-05-15 21:58:09 -0300

    test: update default e2e spec to support global api prefix and basic product fetch

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,8 @@
+/// <reference types="jest" />
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -12,13 +14,15 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api');
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/api/products (GET)', () => {
+    return request(app.getHttpServer()).get('/api/products').expect(200);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
