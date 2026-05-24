3f12397c3972c221d863b080d7b4866d1c245ea8
Emiliano Merelez <emimerelez@gmail.com>
2026-05-24 02:20:34 -0300

    fix: add production ssl config for supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,10 @@ import { SeedModule } from './seed/seed.module';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
+      ssl:
+        process.env.DB_HOST !== 'localhost'
+          ? { rejectUnauthorized: false }
+          : false,
     }),
 
     ProductsModule,
